184eb7dce283fb51bb43fb4a18f5e883aef76bc1
Dmitry Kupiyanov <kuban1408@gmail.com>
2019-12-26 12:46:50 +0400

    initial commit

---
+++ ModuleApp/Source/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ModuleApp
+//
+//  Created by Дмитрий Куприянов on 26.12.2019.
+//  Copyright © 2019 Cooper. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ ModuleApp/Source/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModuleApp
+//
+//  Created by Дмитрий Куприянов on 26.12.2019.
+//  Copyright © 2019 Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
